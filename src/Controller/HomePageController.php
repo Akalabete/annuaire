a7726ceaafe75886ec809ae819c981736c069aa6
Akalabete <lacour_alexandre@hotmail.com>
2025-09-02 09:58:28 +0200

    initial commit annuaire cdc with symfony

---
+++ src/Controller/HomePageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+final class HomePageController extends AbstractController {
+    #[Route('/', name: 'home')]
+
+    function index (Request $request): Response {
+        return new Response('Hello World');
+    }
+}
